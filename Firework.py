165f572ee0f36203fdf75b4ecdf10557f8070842
Fergal Mullally (Dunalmu) <fergal.mullally@gmail.com>
2012-04-16 21:27:05 -0700

    Initial commit. Code works but could use some sprucing up.

---
+++ Firework.py
@@ -0,0 +1,56 @@
+
+import random
+import pygame
+from pygame.color import Color
+
+
+
+class RingFirework():
+    #@TODO surface is set as part of init
+    def __init__(self, surface):
+        self.surface = surface
+
+        x = min(surface.get_size())
+        self.maxSize = random.gauss( int(.18*x), int(.125*x))
+
+        self.innerRadius = 0
+        self.outerRadius = 1
+        speed = random.randrange(20, 30)
+        self.radiusStep = int(self.maxSize/speed)
+        
+        red = random.randrange(64, 256, 64) #Off, on or half on
+        green = random.randrange(64, 256, 64)
+        blue = random.randrange(64, 256, 64)
+        self.colour = Color(red, green, blue)
+
+        (mxx, mxy) = surface.get_size()
+        self.xpos = random.randrange(mxx)
+        self.ypos = random.randrange(mxy)
+        self._isActive = True
+
+    def __str__(self):
+        msg="Firework at (%i,%i) with size %i" \
+            %(self.xpos, self.ypos, self.maxSize)
+        return msg
+
+    def __repr__(self):
+        return __str__()
+        
+    def draw(self):
+        if self.outerRadius < self.maxSize:
+            self.outerRadius += self.radiusStep
+        else:
+            self.innerRadius += self.radiusStep
+
+        pos = [self.xpos, self.ypos]
+        pygame.draw.circle(self.surface, self.colour, pos, self.outerRadius)
+
+        if self.innerRadius > 0:
+            black = Color(0,0,0)
+            pygame.draw.circle(self.surface, black, pos, self.innerRadius)
+
+        if self.innerRadius > self.maxSize+1:
+            self._isActive = False
+
+    def isActive(self):
+        return self._isActive
---
+++ main.py
@@ -0,0 +1,76 @@
+
+import pygame
+from pygame.locals import *
+import Firework 
+
+
+def main():
+    pygame.init()
+    #Find screen size and pass to this function
+    screenSize=pygame.display.list_modes()[0]
+    window = pygame.display.set_mode(screenSize, FULLSCREEN)
+    #window = pygame.display.set_mode((640,480))    #Useful for debugging
+    pygame.display.set_caption('Fireworks!')
+
+    surface = pygame.display.get_surface()
+
+    j=0
+    t1= 0
+    flag, flag2 =True, False
+    objList = []
+    maxObjects = 20
+    while flag:
+        eventList = pygame.event.get(KEYDOWN)
+        pygame.event.get()  #Throw away everything else
+        #print "Eventlist",
+        #print eventList
+        for e in eventList:
+            if e.key == K_ESCAPE:
+                flag2 = True
+                t1 = pygame.time.get_ticks()
+                font = pygame.font.SysFont("Courier New", 18)
+                s = font.render("Hit [Y] to Quit", 1, pygame.color.Color(255, 255, 255))
+                surface.blit(s , (100, 100))
+
+            if flag2:
+                #Quit if K_Y pressed within 5 seconds of flag2 being
+                #raised, otherwise unset flag2 
+                if e.key == K_y:
+                    flag = False
+                elif pygame.time.get_ticks() - t1 > 5000:
+                    flag2 = False
+
+            if len(objList) < maxObjects:
+                f = Firework.RingFirework(surface)
+                objList.append(f)
+            else:
+                print "Max objects exceeded"
+
+        #print "%i new fireworks => %i objects" %(len(eventList), len(objList))
+        #Update fireworks
+        deadList = range(len(objList))
+        for i, obj in enumerate(objList):
+            obj.draw()
+            if obj.isActive():
+                deadList[i] = 0
+            else:
+                deadList[i] = 1
+
+        #Delete dead fireworks
+        #print deadList
+        for i in range(len(deadList)):
+            if deadList[i]>0:
+                objList.pop(i)
+                i=0
+                break #Only remove one object at a time
+        #print "Object list now has %i obj" %(len(objList))
+
+        
+        pygame.display.flip()        
+        pygame.time.wait(40)
+        #raw_input('%i: Hit RETURN' %(j))
+        j+=1
+    
+
+if __name__ == "__main__":
+    main()
